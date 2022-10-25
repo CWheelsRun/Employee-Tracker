c7dc3991c72b4f48ab5970174d6f884019b9be51
Corey B <corey.bennett@live.com>
2022-10-25 14:05:07 -0400

    beginning function of server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the Employee Database!`)
+);
+
+mainPrompt = () => {
+  inquirer.prompt({
+      type: "list",
+      name: "menu",
+      message: "\nWhat would you like to do?\n",
+      choices: [
+        "View all Departments",
+        "View all Roles",
+        "View all Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update Employee Role",
+        "Exit Application",
+      ],
+    })
+    .then((choice) => {
+      switch (choice.menu) {
+        case "View all Departments":
+          viewDepartments();
+          break;
+        case "View all Roles":
+          viewRoles();
+          break;
+        case "View all Employees":
+          viewEmployees();
+          break;
+        case "Add a Department":
+          addDepartment();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+        case "Exit Application":
+          process.exit();
+      }
+    });
+};
+
+mainPrompt();
